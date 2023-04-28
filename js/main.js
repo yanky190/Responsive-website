99ef4e91d3a3ed0c408e693965d26fc8c9e0265c
Yekini Opene <yekini190@gmail.com>
2023-04-28 05:38:59 +0100

    services and work

--- js/main.js
+++ js/main.js
@@ -7,9 +7,19 @@ jQuery(document). ready(function(){
     });
 
 
-    $('.text').typed( {
-        strings: ["<strong>i Love </strong> <strong class='primary'>coding.</strong>", "<stong>and to</strong><strong class='primary'>Shear!!.</strong>"],
+    $('.text').typed({
+        strings:["<strong>i Love </strong> <strong class='primary'>coding.</strong>",
+         "<stong>and to</strong><strong class='primary'>Shear!!.</strong>"],
         typeSpeed: 0,
         loop: true
       });
+
+      $('.work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+        enabled: true
+        }
+      });
+
     });
